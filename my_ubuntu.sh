31f03997c1d9bff19f617f7167ac14b49b863622
gpetrini <gpetrinidasilveira@gmail.com>
2020-03-25 16:36:09 -0300

    Updated packages list

--- my_ubuntu.sh
+++ my_ubuntu.sh
@@ -1,53 +1,53 @@
 #!/usr/bin/env bash
 # ----------------------------- VARIÁVEIS ----------------------------- #
 
-PPA_GRAPHICS_DRIVERS="ppa:graphics-drivers/ppa"
-PPA_TEX_STUDIO="ppa:sunderme/texstudio"
-PPA_TESSERACT="ppa:alex-p/tesseract-ocr"
 PPA_ATOM="ppa:webupd8team/atom"
 PPA_CAFFEINE="ppa:caffeine-developers/ppa"
+PPA_GRAPHICS_DRIVERS="ppa:graphics-drivers/ppa"
+PPA_TESSERACT="ppa:alex-p/tesseract-ocr"
+PPA_TEX_STUDIO="ppa:sunderme/texstudio"
 
+REPO_TYPORA="deb https://typora.io/linux ./"
+REPO_VIVALDI="deb https://repo.vivaldi.com/archive/deb/ stable main"
+
+URL_FRANZ="https://github.com/meetfranz/franz/releases/download/v5.4.0/franz_5.4.0_amd64.deb"
 URL_INSYNC="https://d2t3ff60b2tol4.cloudfront.net/builds/insync_3.0.23.40579-bionic_amd64.deb" ## Not shure, review ##
 URL_PCLOUD="https://www.pcloud.com/pt/how-to-install-pcloud-drive-linux.html?download=electron-64"
-URL_RSTUDIO=""
+URL_ZOTERO="https://github.com/retorquere/zotero-deb/releases/download/apt-get/install.sh"
 
 DIRETORIO_DOWNLOADS="$HOME/$USER/Downloads/programas"
 
 PROGRAMAS_PARA_INSTALAR=(
-  snapd
-  ubuntu-restricted-extras ## Codecs ##
-  gdebi-core ## Pre-req RStudio
-  gnome-tweaks
+  atom
   caffeine
   chromium-browser
   flameshot ## Screenshot
-  gretl
-  pspp
-  r-base
-  libcanberra-gtk-module ## Pre-req popcorntime
-  libgconf-2-4 ## Pre-req popcorntime
-  vim
-  atom
-  texstudio
-  texlive-latex-extra
+  gdebi-core ## Pre-req RStudio
+  gnome-tweaks
   ghostscript ## Pre-req ocrmypdf
+  gretl
+  insync
   libexempi3 ## Pre-req ocrmypdf
   libffi6 ## Pre-req ocrmypdf
   pngquant ## Pre-req ocrmypdf
   qpdf ## Pre-req ocrmypdf
+  r-base
+  snapd
   tesseract-ocr ## Pre-req ocrmypdf
+  texstudio
+  texlive-latex-recommended
+  texlive-latex-extra
+  texlive-publishers
+  texlive-science
+  texlive-humanities
+  texlive-font-utils
+  texlive-bibtex-extra
+  texlive-lang-portuguese
+  typora
+  vivaldi-stable
+  ubuntu-restricted-extras ## Codecs ##
   unpaper ## Pre-req ocrmypdf
-
-
-  libgnutls30:i386
-  libldap-2.4-2:i386
-  libgpg-error0:i386
-  libxml2:i386
-  libasound2-plugins:i386
-  libsdl2-2.0-0:i386
-  libfreetype6:i386
-  libdbus-1-3:i386
-  libsqlite3-0:i386
+  zotero
 )
 # ---------------------------------------------------------------------- #
 
@@ -59,11 +59,22 @@ sudo rm /var/cache/apt/archives/lock
 ## Adicionando/Confirmando arquitetura de 32 bits ##
 sudo dpkg --add-architecture i386
 
+## Adicionando Keys ##
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys ACCAF35C ## Insync
+wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add - ## typora
+
+
+## Criando pastas ##
+deb http://apt.insync.io/ubuntu focal non-free contrib ## Check
+
 ## Atualizando o repositório ##
 sudo apt update -y
 
-## Adicionando repositórios de terceiros (Driver Logitech, Lutris e Drivers Nvidia)##
+## Adicionando repositórios de terceiros##
 sudo apt-add-repository "$PPA_GRAPHICS_DRIVERS" -y
+sudo apt-add-repository "$REPO_TYPORA" -y
+sudo apt-add-repository "$REPO_VIVALDI" -y
+
 # ---------------------------------------------------------------------- #
 
 # ----------------------------- EXECUÇÃO ----------------------------- #
@@ -74,9 +85,14 @@ sudo apt update -y
 mkdir "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_INSYNC"              -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_PCLOUD"              -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_FRANZ"              -P "$DIRETORIO_DOWNLOADS"
+wget -qO- "$URL_ZOTERO"  -P "$DIRETORIO_DOWNLOADS" | sudo bash
+
+
 
 ## Instalando pacotes .deb baixados na sessão anterior ##
 sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
+sudo apt install -f
 
 # Instalar programas no apt
 for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
@@ -87,15 +103,42 @@ for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
   fi
 done
 
-curl https://rclone.org/install.sh | sudo bash ## RClone
-
 
 ## Instalando pacotes Snap ##
-sudo snap install spotify
 sudo snap install drawio
 sudo snap install okular 
 sudo snap install skype --classic
+sudo snap install spotify
+sudo snap install telegram-desktop
 # ---------------------------------------------------------------------- #
+
+
+## Pacotes pyton
+PACOTES_PYTHON=(
+	datetime
+	jupyterlab
+	matplotlib
+	notebook
+	numba
+	numpy	
+	ocrmypdf
+	pandas
+	pandas-datareader
+	pysolve3
+	rpy2
+	scipy
+	seaborn
+	statsmodels
+	sympy
+	twine ## Compilar pacote
+	wheel ## Compilar pacote
+)
+
+for pacote in ${PACOTES_PYTHON[@]}; do
+  sudo -H pip3 install --user "$pacote"
+  echo "[INSTALADO] - $pacote"
+done
+
 
 # ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
 ## Finalização, atualização e limpeza##
@@ -107,10 +150,7 @@ sudo apt autoremove -y
 
 PROGRAMAS_NAO_INSTALADOS=(
 	RStudio
-	Franz
-	Telegram
-	Vivaldi
-	Popcorntime
+	InternetBanking
 )
 
 echo "Instalação automatizada encerrada"
@@ -118,30 +158,5 @@ echo "Instalação automatizada encerrada"
 for nome_do_programa in ${PROGRAMAS_NAO_INSTALADOS[@]}; do
   echo "Seguir para a instalação de  $nome_do_programa"
 done
-
-
-## Pacotes pyton
-PACOTES_PYTHON=(
-	ocrmypdf
-	pandas
-	sympy
-	numpy
-	matplotlib
-	seaborn
-	pysolve3
-	statsmodels
-	scipy
-	numba
-	pandas-datareader
-	rpy2
-	wheel ## Compilar pacote
-	twine ## Compilar pacote
-)
-
-for pacote in ${PACOTES_PYTHON[@]}; do
-  pip3 install --user "$nome_do_programa"
-  echo "[INSTALADO] - $nome_do_programa"
-done
-
 
 
